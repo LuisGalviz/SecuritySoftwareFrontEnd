c1e9832ecbffab8c876c754558fb9543ee417359
Luis Fernando Galviz <lgalviz@innovapps.net>
2024-09-18 22:43:19 -0500

    feat: add yup for login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,67 +1,110 @@
 // src/components/Login.jsx
-import React, { useState } from "react";
-import { TextField, Button, Container, Typography, Box } from "@mui/material";
+import React from "react";
+import {
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Box,
+  FormControl,
+  FormHelperText,
+} from "@mui/material";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import api from "../services/api";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [formData, setFormData] = useState({ username: "", password: "" });
   const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await api.login(formData);
-      alert("Inicio de sesión exitoso");
-      navigate("/dashboard");
-    } catch (error) {
-      console.error("Error al iniciar sesión:", error);
-      alert("Error al iniciar sesión");
-    }
-  };
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required("El nombre de usuario es requerido"),
+      password: Yup.string().required("La contraseña es requerida"),
+    }),
+    onSubmit: async (values) => {
+      try {
+        await api.login(values);
+        alert("Inicio de sesión exitoso");
+        navigate("/dashboard");
+      } catch (error) {
+        console.error("Error al iniciar sesión:", error);
+        formik.setFieldError(
+          "password",
+          "Nombre de usuario o contraseña incorrectos"
+        );
+      }
+    },
+  });
 
   return (
-    <Container maxWidth="xs">
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          mt: 4,
-        }}>
-        <Typography variant="h5">Iniciar Sesión</Typography>
-        <form onSubmit={handleSubmit}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Nombre de Usuario"
-            name="username"
-            onChange={handleChange}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Contraseña"
-            type="password"
-            name="password"
-            onChange={handleChange}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}>
-            Iniciar Sesión
-          </Button>
-        </form>
-      </Box>
-    </Container>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}>
+      <Container maxWidth="xs">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            mt: 4,
+          }}>
+          <Typography variant="h5">Iniciar Sesión</Typography>
+          <form onSubmit={formik.handleSubmit}>
+            <FormControl
+              fullWidth
+              margin="normal">
+              <TextField
+                required
+                label="Nombre de Usuario"
+                name="username"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.username}
+                error={
+                  formik.touched.username && Boolean(formik.errors.username)
+                }
+                helperText={formik.touched.username && formik.errors.username}
+                variant="outlined"
+              />
+            </FormControl>
+            <FormControl
+              fullWidth
+              margin="normal">
+              <TextField
+                required
+                label="Contraseña"
+                type="password"
+                name="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+                variant="outlined"
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}>
+              Iniciar Sesión
+            </Button>
+          </form>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
