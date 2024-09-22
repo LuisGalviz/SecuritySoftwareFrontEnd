df1da9f8361f7485736b2192ef186f991abfcde1
Luis Fernando Galviz <lgalviz@innovapps.net>
2024-09-21 20:09:37 -0500

    fix: security vercel

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 // Configuración de Firebase con variables de entorno
 const firebaseConfig = {
@@ -18,4 +17,3 @@ const app = initializeApp(firebaseConfig);
 // Servicios de Firebase
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
